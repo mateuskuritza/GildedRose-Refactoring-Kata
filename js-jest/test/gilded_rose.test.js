cba4a83e5ccd7aad4e770d86c298fc3725f46a69
mateuskuritza <mateuskuritza@gmail.com>
2021-09-17 14:38:52 -0300

    test: refactored shop should be equal original shop

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,15 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const { Shop, Item, refactoredShop } = require("../src/gilded_rose");
+const { TextTest } = require("./texttest_fixture");
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+describe("Gilded Rose", function () {
+	it("should foo", function () {
+		const gildedRose = new Shop([new Item("foo", 0, 0)]);
+		const items = gildedRose.updateQuality();
+		expect(items[0].name).toBe("foo");
+	});
+	it("new shop shold be equal original shop", function () {
+		const originalShopResult = TextTest(Shop);
+		const refactoredShopResult = TextTest(refactoredShop);
+		expect(originalShopResult).toBe(refactoredShopResult);
+	});
 });
